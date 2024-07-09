d40793dc33ad4256f4cf838b31c6a63a31336cb9
Avanthikaa7 <cavanthika25@gmail.com>
2024-07-09 16:18:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('bud').addEventListener('click',function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2023/03/16/16/49/magnolia-7857101_1280.jpg"
+    });
+document.getElementById('blossom').addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1961778905/photo/pink-cherry-blossom.jpg?s=1024x1024&w=is&k=20&c=ItdyPwhvUVedACRzkvrGJOc8-8R4BFLltRLwy3hJ55M="
+})
